3274f7d0adcb116d9a2d20555fffd3e83c332ac5
yoshihiro <y.kadono4218@gmail.com>
2024-05-26 17:51:39 +0900

    とりあえずLambdaでサンプルニュースが要約できることを確認済

--- src/main/java/com/kadowork/app/AiRadio.java
+++ src/main/java/com/kadowork/app/AiRadio.java
@@ -42,6 +42,22 @@ public class AiRadio implements RequestHandler<Map<String, Object>, Object> {
     private static final String LINE_REPLY_POST_URL = "https://api.line.me/v2/bot/message/reply";
     private static final String DYNAMODB_URL = "https://dynamodb.ap-northeast-1.amazonaws.com";
 
+    private static final String SYSTEM_PROMPT = """
+            あなたはプロの編集者です。与えられたニュース記事を、要点を押さえたうえで、ラジオのアナウンサーがヘッドラインとして読む原稿として要約をします。
+            ニュースのヘッドラインを聞く人たちは専門知識を持っていません。内容が理解しやすいようにしてください。
+            なおラジオのニュースを聞く人たちは日本人なので、日本語で要約をしてください。
+            """;
+    private static final String OTEHON_NEWS = """
+            Title: "Britain to World: Don’t Let What Happened to Us Happen to You",
+            Description: "In bid for re-election, Conservatives’ economic record is hobbled by bad luck and bad choices."
+            Content: "British Prime Minister Rishi Sunak called a general election for July 4. - Martyn Wheatley/Zuma Press President Biden, fighting for re-election with abysmal approval ratings, can count himself lucky… [+6081 chars]"
+            """;
+    private static final String OTEHON_SUMMARY = """
+            イギリスのリシ・スナク首相は、7月4日に総選挙を実施すると発表しました。
+            保守党は、経済実績が悪運や悪い選択によって損なわれている状況に直面しています。
+            一方、アメリカではジョー・バイデン大統領が非常に低い支持率の中、再選に向けて戦っていますが、まだ幸運であると見られています。
+            """;
+
     @Override
     public Object handleRequest(Map<String, Object> map, Context context) {
         // test
@@ -49,41 +65,47 @@ public class AiRadio implements RequestHandler<Map<String, Object>, Object> {
         map.forEach((key, value) -> System.out.println(key + ":" + value));
 
         // TODO とりあえず line のヤツを持ってきたので、 ai-radio 用に作り替えていく格好
-
-//        List<Chat> chatHistory = new ArrayList<>();
-//        Chat chat = Chat.builder()
-//                        .id(UUID.randomUUID().toString())
-//                        .userId(body.getEvents()[0].getSource().getUserId())
-//                        .role(user)
-//                        .content(messageWithoutOption)
-//                        .typedAt(LocalDateTime.now(ZoneId.of("Asia/Tokyo")).toString())
-//                        .build();
-//        chatRepository.save(chat);
-//        chatHistory.add(chat);
+        // テスト
+        List<Chat> chatHistory = new ArrayList<>();
+        Chat chat = Chat.builder()
+                        .id(UUID.randomUUID().toString())
+                        .userId("N/A")
+                        .role(user)
+                        // TODO
+                        .content("""
+                                 Title: Meet Salim Ramji, Who Is Going to Oversee the Retirement Assets of Tens of Millions of Americans
+                                 Description: The first outsider to run Vanguard needs to win over the Bogleheads. Colleagues say he can.
+                                 Content: The first outsider to take the helm of investing giant Vanguard Group is the son of Tanzanian immigrants who didnt attend college but wanted to ensure their three children did. Salim Ramji grew up in… [+5571 chars]
+                                 """)
+                        .typedAt(LocalDateTime.now(ZoneId.of("Asia/Tokyo")).toString())
+                        .build();
+        chatHistory.add(chat);
+        chatOpenAI(chatHistory);
 
         System.out.println("処理終了〜");
         return null;
     }
 
-    private String chatOpenAI(List<Chat> messages) {
+    private void chatOpenAI(List<Chat> messages) {
         final var service = new OpenAiService(OPENAI_TOKEN, Duration.ofSeconds(OPENAPI_DURATION_SECONDS));
         System.out.println("\nCreating completion...");
         List<ChatMessage> chatMessages = new LinkedList<>();
-        chatMessages.add(new ChatMessage("system", "あなたはとても優秀な秘書です。ユーザーからの相談や疑問に対して、親身になって回答してください。その際は説明が長くなりすぎず、より簡潔に内容をまとめるようにしてください。"));
+        chatMessages.add(new ChatMessage("system", SYSTEM_PROMPT));
+        chatMessages.add(new ChatMessage("user",OTEHON_NEWS ));
+        chatMessages.add(new ChatMessage("assistant", OTEHON_SUMMARY));
         messages.stream()
                 .sorted(Comparator.comparing(Chat::getTypedAt))
                 .forEach(x -> chatMessages.add(new ChatMessage(x.getRole().toString(), x.getContent())));
         System.out.println(chatMessages);
         final var chatCompletionRequest = ChatCompletionRequest.builder()
                                                                .model(OPENAI_MODEL_NAME)
-                                                               .maxTokens(1024)
+                                                               .maxTokens(3000)
                                                                .messages(chatMessages)
                                                                .build();
         var result = service.createChatCompletion(chatCompletionRequest);
         for (final ChatCompletionChoice choice : result.getChoices()) {
             System.out.println(choice);
         }
-        return result.getChoices().get(0).getMessage().getContent();
     }
 
     private ResponseEntity<String> postLineNotify(String bodyJson) {
